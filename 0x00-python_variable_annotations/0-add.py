ccb7a4ae8b9918a4258511e181d627a357d940cf
shaco-star <sherifm846@gmail.com>
2023-12-07 00:58:38 -0800

    commit

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+    """_add 2 floats
+    """
+def add(float: a, float: b) ->float:
+    """_summary_
+
+    Args:
+        float (a): float number
+        float (b): float number
+
+    Returns:
+        float: float
+    """
+    return a + b
