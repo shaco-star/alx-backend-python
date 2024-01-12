54f226527056be67cd0195ddf0f1030bef135f91
shaco-star <sherifm846@gmail.com>
2024-01-12 11:30:00 -0800

    commit

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,13 @@
+import unittest
+from parameterized import parameterized
+import utils
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        self.assertEqual(utils.access_nested_map(nested_map, path), expected)
